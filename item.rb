e4e962c5edf6e5d5858ca4d5e7fd4a51adfb8da8
Benny <bennyxnguyen@gmail.com>
2016-06-05 15:21:18 -0400

    completed receipt

--- item.rb
+++ item.rb
@@ -1,48 +1,30 @@
-# sales tax = 10% on everything except books, food, medical products
-# additional 5% import duty on anything imported
-
-class Cart
-
-  def initialize
-    @items = []
-  end
-
-  def add(item)
-    @items << item
-  end
-
-end
-
 class Item
 
-  attr_reader :calculated_tax
+attr_accessor :sales_tax, :import_tax, :shelf_price
+attr_reader :quantity, :name
 
-  def initialize (name, shelf_price, sales_tax, import_tax)
+  def initialize(quantity, name, shelf_price, sales_tax, import_tax)
+    @quantity = quantity
     @name = name
     @shelf_price = shelf_price
     @sales_tax = sales_tax
     @import_tax = import_tax
-    @calculated_tax = 0
   end
 
   def tax
-    sale_tax = 0.10
+    sales_tax = 0.10
     import_tax = 0.05
 
-    if @sale_tax == true && @import_tax == true
-      @calculated_tax = @shelf_price * (sale_tax + import_tax))
+    if @sales_tax == true && @import_tax == true
+      @shelf_price * (sales_tax + import_tax)
+    elsif @sales_tax == true && @import_tax == false
+      @shelf_price + sales_tax
+    elsif @sales_tax == false && @import_tax == true
+      @shelf_price + import_tax
+    elsif @sales_tax == false && @import_tax == false
+      @shelf_price.round
 
-    elsif @sale_tax == true && @import_tax == false
-      @calculated_tax = @shelf_price * (@shelf_price + sale_tax)
-
-    elsif @sale_tax == false && @import_tax == true
-      @calculated_tax = @shelf_price + import_tax
-
-    elsif @sale_tax == false && @import_tax == false
-      @calculated_tax
     end
-
-    return @calculated_tax.round(2)
   end
 
   def total_price
@@ -51,18 +33,59 @@ class Item
 
 end
 
+class Reciept
 
-book = Item.new("book", 12.49, false, false)
-puts book.tax
-music = Item.new("music", 16.49, true, false)
-puts music.tax
-imported_perfume = Item.new("imported_perfume", 32.19, true, true)
-puts imported_perfume.tax
+    attr_accessor :items, :total, :total_sales_tax
 
-cart = Cart.new
-cart.add(book)
-cart.add(music)
-cart.add(imported_perfume)
+    def initialize
+      @items = []
+      @total = 0
+      @total_sales_tax = 0
+    end
 
-puts music.tax
-puts music
+    def add(item)
+      if item.is_a?(Item)
+        @items << item
+        @total_sales_tax += item.tax
+        @total += item.total_price
+      else
+        "Not a class Item"
+      end
+    end
+
+    def printout
+    items.each do |item|
+      p "#{item.quantity} #{item.name}: #{(item.shelf_price).round(2)}"
+    end
+      p "Sales Tax: #{(total_sales_tax).round(2)}"
+      p "Total: #{(total).round(2)}"
+
+  end
+end
+
+
+book = Item.new( 1, "book", 12.49, false, false )
+music_cd = Item.new( 1, "music cd", 14.99, true, false)
+chocolate_bar = Item.new( 1, "chocolate bar", 0.85, false, false)
+box_of_chocolates = Item.new(1, "imported box of chocolate", 10.00, false, true)
+imported_perfume = Item.new(1, "imported bottle of perfume", 47.50, true, true)
+perfume = Item.new(1, "bottle of perfume", 20.89, true, false)
+headache_pills = Item.new(1, "packet of headache pills", 9.75, false, false)
+
+reciept1 = Reciept.new
+reciept1.add(book)
+reciept1.add(music_cd)
+reciept1.add(chocolate_bar)
+reciept1.printout
+
+reciept2 = Reciept.new
+reciept2.add(box_of_chocolates)
+reciept2.add(imported_perfume)
+reciept2.printout
+
+reciept3 = Reciept.new
+reciept3.add(perfume)
+reciept3.add(box_of_chocolates)
+reciept3.add(imported_perfume)
+reciept3.add(headache_pills)
+reciept3.printout
--- receipt.rb
+++
@@ -1,4 +0,0 @@
-# sales tax = 10% on everything except books, food, medical products
-# additional 5% import duty on anything imported
-class Goods
-  
